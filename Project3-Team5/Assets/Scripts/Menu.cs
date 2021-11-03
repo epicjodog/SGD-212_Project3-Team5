325b2f521aef5f24b2931e5b3258c2f50c2857a0
RedLine2021 <pjosephrichard97@gmail.com>
2021-11-03 19:53:49 -0400

    Added pause menu
    Added pause menu

--- Project3-Team5/Assets/Scripts/Menu.cs
+++ Project3-Team5/Assets/Scripts/Menu.cs
@@ -5,7 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject pauseMenu;
 
+    public bool isPaused;
     public void playGame(int SceneIndex)
     {
         SceneManager.LoadScene("Level 1");//Will switch between menu to level 1 
@@ -13,12 +15,45 @@ public class Menu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        pauseMenu.SetActive(false);//When game starts pause menu will not start up as well
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))//If Esc is press pause menu will pop up and pause game
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);//Turns on pause menu
+        Time.timeScale = 0f;// stops game
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);//turns off pause menu
+        Time.timeScale = 1f;//continues game
+        isPaused = false;
+    }
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
