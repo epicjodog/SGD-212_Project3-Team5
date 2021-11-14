fc054d86224271a4e8c62a1c8b139fe1510abe79
epicjodog <epicjodog@gmail.com>
2021-11-14 01:23:42 -0500

    Improved main menu UI
    Also added some improvements with a handful of functions in Menu.cs (I kept the original ones so you can easily transfer)

--- Project3-Team5/Assets/Scripts/Menu.cs
+++ Project3-Team5/Assets/Scripts/Menu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
@@ -11,13 +12,25 @@ public class Menu : MonoBehaviour
     public static bool isPaused;
 
     [Header("Main Menu")]
-    public GameObject helpPanel;
-    public GameObject[] helpPanelPages;
-    public GameObject creditsPanel;
+    [SerializeField] bool isMainMenu = false;
+    //Level select
+    [SerializeField] GameObject levelSelectPanel;
+    [SerializeField] Button[] levelSelectButtons;
+    //Help
+    [SerializeField] GameObject helpPanel;
+    [SerializeField] GameObject[] helpPanelPages;
+    //Credits
+    [SerializeField] GameObject creditsPanel;
+    [SerializeField] GameObject[] creditsPanelPages;
 
     int helpPanelCurrentPage = 0;
+    int creditsPanelCurrentPage = 0;
 
-
+    public void loadLevel(string levelName) //migrate all the ui to this function
+    {
+        //later, add an if statement seeing if the level has been unlocked.
+        SceneManager.LoadScene(levelName);
+    }
     public void playGame(int SceneIndex)
     {
         SceneManager.LoadScene("Level 1");//Will switch between menu to level 1 
@@ -27,16 +40,24 @@ public class Menu : MonoBehaviour
     {
         SceneManager.LoadScene("Level 2");//Will switch between menu to level 1 
     }
+
     // Start is called before the first frame update
     void Start()
     {
+        if(isMainMenu)
+        {
+            levelSelectPanel.SetActive(false);
+            helpPanel.SetActive(false);
+            creditsPanel.SetActive(false);
+        }
         pauseMenu.SetActive(false);//When game starts pause menu will not start up as well
+        //check to see if levels are unlocked. if not, disable specific buttons
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))//If Esc is press pause menu will pop up and pause game. frees up the cursor too.
+        if (Input.GetKeyDown(KeyCode.Escape) && !isMainMenu)//If Esc is press pause menu will pop up and pause game. frees up the cursor too.
         {
             if (isPaused)
             {
@@ -66,6 +87,26 @@ public class Menu : MonoBehaviour
         Time.timeScale = 1f;//continues game
         isPaused = false;
     }
+
+    void PauseManager()
+    {
+        if(!isPaused) //PauseGame()
+        {
+            pauseMenu.SetActive(true);//Turns on pause menu
+            Time.timeScale = 0f;// stops game
+            isPaused = true;
+            Cursor.visible = true;//Cursor is seen
+            Cursor.lockState = CursorLockMode.None;//Cursor can move around freely and not stuck in place
+        }
+        else //ResumeGame()
+        {
+            pauseMenu.SetActive(false);//turns off pause menu
+            Time.timeScale = 1f;//continues game
+            isPaused = false;
+            Cursor.lockState = CursorLockMode.Locked;//Cursor is stuck in place
+            Cursor.visible = false;//Cursor is invisible 
+        }
+    }
     public void GoToMainMenu()
     {
         Time.timeScale = 1f;
@@ -75,6 +116,20 @@ public class Menu : MonoBehaviour
     {
         Application.Quit();
     }
+    public void LevelSelect()
+    {
+        if (levelSelectPanel.activeSelf == true)
+        {
+            levelSelectPanel.SetActive(false);
+        }
+        else
+        {
+            levelSelectPanel.SetActive(true);
+            helpPanel.SetActive(false);
+            creditsPanel.SetActive(false);
+        }
+    }
+    //Logic for main menu Help Buttons
     public void HelpButton()
     {
         if(helpPanel.activeSelf == true)
@@ -83,7 +138,9 @@ public class Menu : MonoBehaviour
         }
         else
         {
+            levelSelectPanel.SetActive(false);
             helpPanel.SetActive(true);
+            creditsPanel.SetActive(false);
         }
     }
     public void HelpButtonTurnPage(bool isForward)
@@ -91,27 +148,21 @@ public class Menu : MonoBehaviour
         helpPanelPages[helpPanelCurrentPage].SetActive(false);
         if (isForward)
         {
-            helpPanelCurrentPage++;
+            if(helpPanelCurrentPage < helpPanelPages.Length - 1)
+            {
+                helpPanelCurrentPage++;
+            }          
         }
         else
         {
-            helpPanelCurrentPage--;
+            if(helpPanelCurrentPage > 0)
+            {
+                helpPanelCurrentPage--;
+            }           
         }
         helpPanelPages[helpPanelCurrentPage].SetActive(true);
-    }
-    /*public void CreditButtonTurnPage(bool isForward)
-    {
-        helpPanelPages[helpPanelCurrentPage].SetActive(false);
-        if (isForward)
-        {
-            helpPanelCurrentPage++;
-        }
-        else
-        {
-            helpPanelCurrentPage--;
-        }
-        helpPanelPages[helpPanelCurrentPage].SetActive(true);
-    }*/
+    } 
+    //Logic for main menu Credits Buttons
     public void CreditsButton()
     {
         if(creditsPanel.activeSelf == true)
@@ -120,7 +171,28 @@ public class Menu : MonoBehaviour
         }
         else
         {
+            levelSelectPanel.SetActive(false);
+            helpPanel.SetActive(false);
             creditsPanel.SetActive(true);
         }
+    }
+    public void CreditButtonTurnPage(bool isForward) //something that I thought of for the pause menu
+    {
+        creditsPanelPages[creditsPanelCurrentPage].SetActive(false);
+        if (isForward)
+        {
+            if(creditsPanelCurrentPage < creditsPanelPages.Length - 1)
+            {
+                creditsPanelCurrentPage++;
+            }           
+        }
+        else
+        {
+            if(creditsPanelCurrentPage > 0)
+            {
+                creditsPanelCurrentPage--;
+            }
+        }
+        creditsPanelPages[creditsPanelCurrentPage].SetActive(true);
     }
 }
